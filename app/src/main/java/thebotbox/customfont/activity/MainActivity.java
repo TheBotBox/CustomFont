f91b64ee0bc3f456cb26c14e772e9b9853bfe2dc
TheBotBox <boxforbot@gmail.com>
2018-09-01 22:37:40 +0530

    updating list of calligraphy fonts

--- app/src/main/java/thebotbox/customfont/activity/MainActivity.java
+++ app/src/main/java/thebotbox/customfont/activity/MainActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.widget.TextView;
 
-import bot.box.customfont.CustomFont;
-import bot.box.customfont.CustomFontFactory;
+import bot.box.customfont.Fuente;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import thebotbox.customfont.R;
@@ -15,57 +14,6 @@ public class MainActivity extends BaseActivity {
 
     @BindView(R.id.agency_r)
     TextView agency_r;
-
-    @BindView(R.id.agency_b)
-    TextView agency_b;
-
-    @BindView(R.id.star_jedi)
-    TextView star_jedi;
-
-    @BindView(R.id.star_jedi_1)
-    TextView star_jedi_1;
-
-    @BindView(R.id.machine)
-    TextView machine;
-
-    @BindView(R.id.space_age)
-    TextView space_age;
-
-    @BindView(R.id.astro_boy)
-    TextView astro_boy;
-
-    @BindView(R.id.metal_lord)
-    TextView metal_lord;
-
-    @BindView(R.id.saucer)
-    TextView saucer;
-
-    @BindView(R.id.raider_crusader)
-    TextView raider_crusader;
-
-    @BindView(R.id.arkitech_l)
-    TextView arkitech_l;
-
-    @BindView(R.id.arkitech_m)
-    TextView arkitech_m;
-
-    @BindView(R.id.arkitech_b)
-    TextView arkitech_b;
-
-    @BindView(R.id.elektra)
-    TextView elektra;
-
-    @BindView(R.id.alien)
-    TextView alien;
-
-    @BindView(R.id.vtks)
-    TextView vtks;
-
-    @BindView(R.id.droid_nation)
-    TextView droid_nation;
-
-    @BindView(R.id.dameron)
-    TextView dameron;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,37 +28,8 @@ public class MainActivity extends BaseActivity {
     }
 
     private void loadOnViews() {
-        agency_r.setTypeface(CustomFont.amatic_r(this));
-        agency_b.setTypeface(CustomFont.amatic_b(this));
+        agency_r.setTypeface(Fuente.CALLIGRAPHY.abbeyline(this));
 
-        star_jedi.setTypeface(CustomFont.star_jedi(this));
-        star_jedi_1.setTypeface(CustomFont.star_jedi_1(this));
-
-        machine.setTypeface(CustomFont.machine(this));
-
-        space_age.setTypeface(CustomFont.space_age(this));
-
-        astro_boy.setTypeface(CustomFont.astro_boy(this));
-
-        metal_lord.setTypeface(CustomFont.metal_lord(this));
-
-        saucer.setTypeface(CustomFont.saucer(this));
-
-        raider_crusader.setTypeface(CustomFont.raider_crusader(this));
-
-        arkitech_l.setTypeface(CustomFont.arkitech_l(this));
-        arkitech_m.setTypeface(CustomFont.arkitech_m(this));
-        arkitech_b.setTypeface(CustomFont.arkitech_b(this));
-
-        alien.setTypeface(CustomFont.alien(this));
-
-        elektra.setTypeface(CustomFont.elektra(this));
-
-        vtks.setTypeface(CustomFont.vtks(this));
-
-        droid_nation.setTypeface(CustomFont.droid_nation(this));
-
-        dameron.setTypeface(CustomFont.dameron(this));
     }
 
 
--- customfont/src/main/java/bot/box/customfont/Fuente.java
+++ customfont/src/main/java/bot/box/customfont/Fuente.java
@@ -1,5 +1,10 @@
 package bot.box.customfont;
 
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.lang.reflect.Type;
+
 /**
  * Created by Barry Allen .
  * boxforbot@gmail.com
@@ -8,9 +13,299 @@ package bot.box.customfont;
 public class Fuente {
 
     public static class CALLIGRAPHY {
+
+        public static Typeface abbeyline(Context context) {
+            return FuenteFactory.load(R.raw.calli_abbeyline, context);
+        }
+
+        public static Typeface acceleration(Context context) {
+            return FuenteFactory.load(R.raw.calli_acceleration, context);
+        }
+
+        public static Typeface acryle_scrit(Context context) {
+            return FuenteFactory.load(R.raw.calli_acryle_scrit, context);
+        }
+
+        public static Typeface adine(Context context) {
+            return FuenteFactory.load(R.raw.calli_adine, context);
+        }
+
+        public static Typeface admiration_pains(Context context) {
+            return FuenteFactory.load(R.raw.calli_admiration_pains, context);
+        }
+
+        public static Typeface aerolite(Context context) {
+            return FuenteFactory.load(R.raw.calli_aerolite, context);
+        }
+
+        public static Typeface aguafina(Context context) {
+            return FuenteFactory.load(R.raw.calli_aguafina, context);
+        }
+
+        public static Typeface ail_et_fines_herbes(Context context) {
+            return FuenteFactory.load(R.raw.calli_fines_herbes, context);
+        }
+
+        public static Typeface aldebaran(Context context) {
+            return FuenteFactory.load(R.raw.calli_aldebaran, context);
+        }
+
+        public static Typeface alex_brush(Context context) {
+            return FuenteFactory.load(R.raw.calli_alex_brush, context);
+        }
+
+        public static Typeface aligot_de_mirabelle(Context context) {
+            return FuenteFactory.load(R.raw.calli_aligot_de_mirabelle, context);
+        }
+
+        public static Typeface anaconda(Context context) {
+            return FuenteFactory.load(R.raw.calli_anaconda, context);
+        }
+
+        public static Typeface anabelle(Context context) {
+            return FuenteFactory.load(R.raw.calli_anabelle, context);
+        }
+
+        public static Typeface amal_regular(Context context) {
+            return FuenteFactory.load(R.raw.calli_amal_regular, context);
+        }
+
+        public static Typeface amal_bold(Context context) {
+            return FuenteFactory.load(R.raw.calli_amal_bold, context);
+        }
+
+        public static Typeface als(Context context) {
+            return FuenteFactory.load(R.raw.calli_als, context);
+        }
+
+        public static Typeface allura(Context context) {
+            return FuenteFactory.load(R.raw.calli_allura, context);
+        }
+
+        public static Typeface angelface(Context context) {
+            return FuenteFactory.load(R.raw.calli_angelface, context);
+        }
+
+        public static Typeface angelique(Context context) {
+            return FuenteFactory.load(R.raw.calli_angelique, context);
+        }
+
+        public static Typeface angelique_rose(Context context) {
+            return FuenteFactory.load(R.raw.calli_angeli_rose, context);
+        }
+
+        public static Typeface angelofblood(Context context) {
+            return FuenteFactory.load(R.raw.calli_angelofblood, context);
+        }
+
+        public static Typeface angels_message(Context context) {
+            return FuenteFactory.load(R.raw.calli_angels_message, context);
+        }
+
+        public static Typeface queen_anha(Context context) {
+            return FuenteFactory.load(R.raw.calli_queen_anha, context);
+        }
+
+        public static Typeface annabel(Context context) {
+            return FuenteFactory.load(R.raw.calli_annabel, context);
+        }
+
+        public static Typeface arabella(Context context) {
+            return FuenteFactory.load(R.raw.calli_arabella, context);
+        }
+
+        public static Typeface arizonia(Context context) {
+            return FuenteFactory.load(R.raw.calli_arizonia, context);
+        }
+
+        public static Typeface brewery(Context context) {
+            return FuenteFactory.load(R.raw.calli_brewery, context);
+        }
+
+        public static Typeface artonic(Context context) {
+            return FuenteFactory.load(R.raw.calli_artonic, context);
+        }
+
+        public static Typeface atlantis(Context context) {
+            return FuenteFactory.load(R.raw.calli_atlantis, context);
+        }
+
+        public static Typeface barbershop(Context context) {
+            return FuenteFactory.load(R.raw.calli_barbershop, context);
+        }
+
+        public static Typeface baroque(Context context) {
+            return FuenteFactory.load(R.raw.calli_baroque, context);
+        }
+
+        public static Typeface bastardilla(Context context) {
+            return FuenteFactory.load(R.raw.calli_bastardilla, context);
+        }
+
+        public static Typeface beaked_tyrant(Context context) {
+            return FuenteFactory.load(R.raw.calli_beaked_tyrant, context);
+        }
+
+        public static Typeface beautiful(Context context) {
+            return FuenteFactory.load(R.raw.calli_beautiful, context);
+        }
+
+        public static Typeface beautiful_creatures(Context context) {
+            return FuenteFactory.load(R.raw.calli_beautiful_creatures, context);
+        }
+
+        public static Typeface beforeTheRain(Context context) {
+            return FuenteFactory.load(R.raw.calli_before_rain, context);
+        }
+
+        public static Typeface berty(Context context) {
+            return FuenteFactory.load(R.raw.calli_berty, context);
+        }
+
+        public static Typeface billion_stars(Context context) {
+            return FuenteFactory.load(R.raw.calli_billion_stars, context);
+        }
+
+        public static Typeface bluelmin(Context context) {
+            return FuenteFactory.load(R.raw.calli_bluelmin, context);
+        }
+
+        public static Typeface bash(Context context) {
+            return FuenteFactory.load(R.raw.calli_bash, context);
+        }
+
+        public static Typeface bolina(Context context) {
+            return FuenteFactory.load(R.raw.calli_bolina, context);
+        }
+
+        public static Typeface bradstone(Context context) {
+            return FuenteFactory.load(R.raw.calli_bradstone, context);
+        }
+
+        public static Typeface brannboll(Context context) {
+            return FuenteFactory.load(R.raw.calli_brannboll, context);
+        }
+
+        public static Typeface brannboll_ny(Context context) {
+            return FuenteFactory.load(R.raw.calli_brannboll_ny, context);
+        }
+
+        public static Typeface brock(Context context) {
+            return FuenteFactory.load(R.raw.calli_brock, context);
+        }
+
+        public static Typeface broken_heart(Context context) {
+            return FuenteFactory.load(R.raw.calli_broken_heart, context);
+        }
+
+        public static Typeface brotherhood(Context context) {
+            return FuenteFactory.load(R.raw.calli_brotherhood, context);
+        }
+
+        public static Typeface brother_tattoo(Context context) {
+            return FuenteFactory.load(R.raw.calli_brother_tattoo, context);
+        }
+
+        public static Typeface bucanera(Context context) {
+            return FuenteFactory.load(R.raw.calli_bucanera, context);
+        }
+
+        public static Typeface calendary_hands(Context context) {
+            return FuenteFactory.load(R.raw.calli_calendary_hands, context);
+        }
+
+        public static Typeface candle_mustard(Context context) {
+            return FuenteFactory.load(R.raw.calli_candle_mustard, context);
+        }
+
+        public static Typeface canela_bark(Context context) {
+            return FuenteFactory.load(R.raw.calli_canela_bark, context);
+        }
+
+        public static Typeface capinella(Context context) {
+            return FuenteFactory.load(R.raw.calli_capinella, context);
+        }
+
+        public static Typeface centeria(Context context) {
+            return FuenteFactory.load(R.raw.calli_centeria, context);
+        }
+
+        public static Typeface champignon(Context context) {
+            return FuenteFactory.load(R.raw.calli_champignon, context);
+        }
+
+        public static Typeface champion_shipmate(Context context) {
+            return FuenteFactory.load(R.raw.calli_champion_shipmate, context);
+        }
+
+        public static Typeface cheddar_jack(Context context) {
+            return FuenteFactory.load(R.raw.calli_cheddar_jack, context);
+        }
+
+        public static Typeface chopin(Context context) {
+            return FuenteFactory.load(R.raw.calli_chopin, context);
+        }
+
+        public static Typeface clicker(Context context) {
+            return FuenteFactory.load(R.raw.calli_clicker, context);
+        }
+
+        public static Typeface close_to_you(Context context) {
+            return FuenteFactory.load(R.raw.calli_close_to_you, context);
+        }
+
+        public static Typeface coneria(Context context) {
+            return FuenteFactory.load(R.raw.calli_coneria, context);
+        }
+
+        public static Typeface coventry(Context context) {
+            return FuenteFactory.load(R.raw.calli_coventry, context);
+        }
+
+        public static Typeface crownheights(Context context) {
+            return FuenteFactory.load(R.raw.calli_crownheights, context);
+        }
+
+        public static Typeface dancing_in_moonlight(Context context) {
+            return FuenteFactory.load(R.raw.calli_dancing_in_moonlight, context);
+        }
+
+        public static Typeface dancing_in_rainbow(Context context) {
+            return FuenteFactory.load(R.raw.calli_dancing_in_rainbow, context);
+        }
+
+        public static Typeface dancing_regular(Context context) {
+            return FuenteFactory.load(R.raw.calli_dancing_regular, context);
+        }
+
+        public static Typeface dancing_bold(Context context) {
+            return FuenteFactory.load(R.raw.calli_dancing_bold, context);
+        }
+
+        public static Typeface dark_roast(Context context) {
+            return FuenteFactory.load(R.raw.calli_dark_roast, context);
+        }
+
+        public static Typeface darleston(Context context) {
+            return FuenteFactory.load(R.raw.calli_darleston, context);
+        }
+
+        public static Typeface dear_joe_four(Context context) {
+            return FuenteFactory.load(R.raw.calli_dear_joe_four, context);
+        }
+
+        public static Typeface death_in_shadow(Context context) {
+            return FuenteFactory.load(R.raw.calli_death_in_shadow, context);
+        }
+
+        public static Typeface diane_de_france(Context context) {
+            return FuenteFactory.load(R.raw.calli_diane_de_france, context);
+        }
+
+
     }
 
-    public static class SCI_FI {
+/*    public static class SCI_FI {
 
     }
 
@@ -39,6 +334,6 @@ public class Fuente {
     }
 
     public static class WESTERN {
-    }
+    }*/
 
 }
